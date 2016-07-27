968ba1c71b41ea308a13ef01675ab04a65669238
xpcomrade <xpcomrade@gmail.com>
2016-07-27 15:21:25 +0800

    delete BaseTest file

--- snpz-rpc/src/test/java/com/xpcomrade/study/snpzrpc/client/HelloServiceTest.java
+++ snpz-rpc/src/test/java/com/xpcomrade/study/snpzrpc/client/HelloServiceTest.java
@@ -3,14 +3,19 @@ package com.xpcomrade.study.snpzrpc.client;
 import com.xpcomrade.study.snpzrpc.sample.HelloService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by xpcomrade on 2016/7/26.
  * Copyright (c) 2016, xpcomrade@gmail.com All Rights Reserved.
  * Description: TODO(这里用一句话描述这个类的作用). <br/>
  */
-public class HelloServiceTest extends BaseTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-snpz-rpc-client.xml")
+public class HelloServiceTest {
 
     @Autowired
     private RpcProxy rpcProxy;
